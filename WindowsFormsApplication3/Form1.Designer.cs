1ecfea23bdecb20c548fabb0d61525606a66cb91
RichardTacadena <MO0nB0y6991@gmail.com>
2017-01-24 14:07:06 +1100

    Add project files.

---
+++ WindowsFormsApplication3/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace WindowsFormsApplication3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pbMove = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pbMove)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbMove
+            // 
+            this.pbMove.Image = ((System.Drawing.Image)(resources.GetObject("pbMove.Image")));
+            this.pbMove.Location = new System.Drawing.Point(593, 326);
+            this.pbMove.Name = "pbMove";
+            this.pbMove.Size = new System.Drawing.Size(100, 50);
+            this.pbMove.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbMove.TabIndex = 0;
+            this.pbMove.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1318, 762);
+            this.Controls.Add(this.pbMove);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pbMove)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbMove;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ WindowsFormsApplication3/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    {
+
+        int xSpeed = 0;
+        int ySpeed = 0;
+        int xAccelLimit = 21;
+        int yAccelLimit = 21;
+        int acceleration = 1;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            pbMove.Left += xSpeed;
+            pbMove.Top += ySpeed;
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyCode == Keys.D)
+            {
+                if(xSpeed<=xAccelLimit)
+                {
+                    xSpeed += acceleration;
+                }
+            }
+
+            if (e.KeyCode == Keys.A)
+            {
+                if (xSpeed >= -xAccelLimit)
+                {
+                    xSpeed -= acceleration;
+                }
+
+            }
+
+            if (e.KeyCode == Keys.S)
+            {
+                if (ySpeed <= yAccelLimit)
+                {
+                    ySpeed += acceleration;
+                }
+            }
+
+            if (e.KeyCode == Keys.W)
+            {
+                if (ySpeed >= -yAccelLimit)
+                {
+                    ySpeed -= acceleration;
+                }
+            }
+
+
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
